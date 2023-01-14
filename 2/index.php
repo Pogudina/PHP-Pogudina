f72c3717fe12386cdcbfb0c4ce6448d890c315af
Pogudina <80483287+Pogudina@users.noreply.github.com>
2023-01-14 13:55:31 +0500

    Commit second lab php

---
+++ 2/index.php
@@ -0,0 +1,24 @@
+<html>
+
+<head> <title> ЛР 2 </title> </head>
+  <a href="http://a0765723.xsph.ru/">Вернуться назад</a>
+
+  <h1>Погудина М.К. </h1>  
+  <h1> Пи-423</h1>  
+<body>
+
+<u1>
+         <h2>Часть 1</h2>
+        <li><a href="http://a0765723.xsph.ru/2/lab-2-1.php">Лаба-2-1</a></li>
+		<li><a href="http://a0765723.xsph.ru/2/lab-2-2.php">Лаба-2-2</a></li>
+		<li><a href="http://a0765723.xsph.ru/2/lab-2-3.php">Лаба-2-3</a></li>
+		<h2>СР</h2>
+		<li><a href="http://a0765723.xsph.ru/2/lab-2-4.php">Лаба-2-4</a></li>
+		<h2>Часть 2</h2>
+        <li><a href="http://a0765723.xsph.ru/2/lab-2-5.php">Лаба-2-5</a></li>
+        <li><a href="http://a0765723.xsph.ru/2/lab-2-6.php">Лаба-2-6</a></li>
+		
+</u1>
+
+</body>
+</html>
